19370600a6a8bdb6ceccf9faefb30d8796dbee9d
ZeddzZ <zhenya_dz@mail.ru>
2015-03-16 18:54:36 +0300

    Add History

--- Chat/homepage/js.js
+++ Chat/homepage/js.js
@@ -1,18 +1,29 @@
 var historyList = [];
 var oldMessage;
 var messageIndex;
-var newNick = "User";
+var newNick;// = "User";
 
 function run() {
-    alert("Nick!");
-    newNick.value = restoreNick();
-    alert("Nick!");
-    document.getElementById("nick").value = newNick.value;
-    alert("Nick!");
-    historyList = restoreHistory();
-    alert("History!");
+    var nickname = localStorage.getItem("nick");
+    var name = nickname.toString().substring(1, nickname.toString().length - 1);
+    document.getElementById("this_nick").value = name;
+var history = [];
+history = localStorage.getItem("history");
+var begin = history.toString().indexOf("\u0022");
+var end = history.toString().lastIndexOf("\u0022");
+var i = 0;
+while(begin != end) {
+var temp = history.toString().indexOf("\u0022", begin+1);
+var historyTemp = history.toString().substring(begin+1, temp);
+if (i % 2 == 0)
+	historyList.push(historyTemp);
+begin = temp;
+i++;
+}
     reloadHistory(); 
-    alert("History!");
+//    alert("History!");
+
+
 }
 function storeNick(newNick) {
     if(typeof(Storage) == "undefined") {
@@ -21,7 +32,7 @@ function storeNick(newNick) {
     }
 	localStorage.setItem("nick", JSON.stringify(newNick));
 }
-
+//?
 function restoreNick() {
     if (typeof (Storage) == "undefined") {
         alert('Failed to restore nickname');
@@ -31,11 +42,12 @@ function restoreNick() {
     return item && JSON.parse(item);
 }
 
-function storeHistory(History) {
+function storeHistory(history) {
     if(typeof(Storage) == "undefined") {
 	    alert('Failed to store history');
 		return;
 	}
+//alert("1");
     localStorage.setItem("history", JSON.stringify(history));
 }
 
@@ -52,22 +64,28 @@ function change_nick() {
     if (document.getElementById("nick").value != "") {
         newNick = document.getElementById("nick").value;
         var NickArea = document.getElementById("nick");
-        var oldNick = document.getElementById("this_nick").value;
+       var oldNick = document.getElementById("this_nick").value;
         document.getElementById("nick").value = "";
         document.getElementById("this_nick").value = newNick;
         alert("Nick changed. New nick is " + newNick);
         storeNick(newNick);
+//	localStorage.setItem('name', JSON.stringify(newNick));
     }
 }
 
 function send() {
     if (document.getElementById("message").value != "") {
+//alert("1");
         var message = document.getElementById("message");
+//alert("2");
         var nick = document.getElementById("this_nick").value;        
-        historyList.push("<" + nick + ">" + ": " + message.value);
+//alert("3");        
+	historyList.push("<" + nick + ">" + ": " + message.value);
+//alert("4");
         reloadHistory();
         message.value = "";
         storeHistory(historyList);
+//alert("66");
     }
 }
 
@@ -125,11 +143,18 @@ function editMessage() {
 }
 
 function endEdit() {
-    historyList[messageIndex - 1] = document.getElementById("message").value + "      //Message edited by " + newNick + "//";
+    historyList[messageIndex - 1] = document.getElementById("message").value + "      //Message edited by " + document.getElementById("this_nick").value + "//";
     document.getElementById("message").value = oldMessage;
     document.getElementById("MessageButton").setAttribute("onclick", "send();");
     document.getElementById("NickButton").removeAttribute("disabled");
     document.getElementById("edit").removeAttribute("disabled");
     document.getElementById("delete").removeAttribute("disabled");
     reloadHistory();
+}
+
+function cleanHistory() {
+	alert(historyList);
+	historyList.length = 0;
+	alert(historyList);
+	reloadHistory();
 }
