ddd2db2a0027a4260c906295b2e579142eb6c332
ZeddzZ <zhenya_dz@mail.ru>
2015-05-19 21:15:22 +0300

    8 Lab Added. Again...

--- Chat/homepage/js.js
+++ Chat/homepage/js.js
@@ -25,6 +25,19 @@ i++;
 
 
 }
+var appState = {
+    mainUrl: 'http://localhost:999/chat',
+    token: 'TE11EN'
+};
+
+function isOnline() {
+    stat = document.getElementById('connection');
+    if (stat.innerHTML == "Connected")
+        stat.innerHTML = "Failed to connect";
+    else if (stat.innerHTML == "Failed to connect")
+        stat.innerHTML = "Connected";
+};
+
 function storeNick(newNick) {
     if(typeof(Storage) == "undefined") {
 	    alert('Failed to store nickname');
@@ -47,7 +60,7 @@ function storeHistory(history) {
 	    alert('Failed to store history');
 		return;
 	}
-//alert("1");
+//alert("123");
     localStorage.setItem("history", JSON.stringify(history));
 }
 
@@ -68,20 +81,37 @@ function change_nick() {
         document.getElementById("nick").value = "";
         document.getElementById("this_nick").value = newNick;
         alert("Nick changed. New nick is " + newNick);
+		var N = new Object();
+		var str = "User '";
+		str += oldNick;
+		str += "' changed nickname to '";
+		str += newNick;
+		str += "'";
+		N.info = str;
+		post(appState.mainUrl, JSON.stringify(N), function() { updateMessage();} );
         storeNick(newNick);
 //	localStorage.setItem('name', JSON.stringify(newNick));
     }
 }
 
 function send() {
+	var mess = new Object();
     if (document.getElementById("message").value != "") {
 //alert("1");
         var message = document.getElementById("message");
 //alert("2");
-        var nick = document.getElementById("this_nick").value;        
+mess.message = message.value;
+        var nick = document.getElementById("this_nick").value;  
+		mess.nick = nick;
+    var id = Math.floor(Math.random()*1000000 + Math.random()*100000);
+	mess.ID = id;
 //alert("3");        
 	historyList.push("<" + nick + ">" + ": " + message.value);
 //alert("4");
+//alert(mess.message + "\n" + mess.nick + "\n" + mess.ID);
+//alert("45");
+post(appState.mainUrl, JSON.stringify(mess), function() { updateMessage();} );
+//alert("5");
         reloadHistory();
         message.value = "";
         storeHistory(historyList);
@@ -158,3 +188,75 @@ function cleanHistory() {
 	alert(historyList);
 	reloadHistory();
 }
+
+function get(url, continueWith, continueWithError) {
+    ajax('GET', url, null, continueWith, continueWithError);
+};
+
+function post(url, data, continueWith, continueWithError) {
+    ajax('POST', url, data, continueWith, continueWithError);
+};
+
+function put(url, data, continueWith, continueWithError) {
+    ajax('PUT', url, data, continueWith, continueWithError);
+};
+
+function deleteF(url, continueWith, continueWithError) {
+    ajax('DELETE', url, null, continueWith, continueWithError);
+};
+
+
+function ajax(method, url, data, continueWith, continueWithError) {
+    statusButton = document.getElementById('connection');
+    var xmlhttp = getXmlHttp();
+    xmlhttp.open(method || 'GET', url, true);
+    xmlhttp.onreadystatechange = function() {
+        if (xmlhttp.readyState == 4) {
+            if (xmlhttp.status == 200) {
+                if (xmlhttp.responseText != "")
+                    continueWith(JSON.parse(xmlhttp.responseText));
+                else
+                    continueWith();
+                statusButton.innerHTML = "Connected";
+            }
+            if (xmlhttp.status == 0)
+                statusButton.innerHTML = "Failed to connect";
+        }
+    };
+    xmlhttp.ontimeout = function() {
+        statusButton.innerHTML = "Failed to connect";
+    };
+    xmlhttp.send(data);
+};
+
+function getXmlHttp() {
+    var xmlhttp;
+    try {
+        xmlhttp = new ActiveXObject("Msxml2.XMLHTTP");
+    } catch(e) {
+        try {
+            xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+        } catch(E) {
+            xmlhttp = false;
+        }
+    }
+    if (!xmlhttp && typeof XMLHttpRequest != 'undefined')
+        xmlhttp = new XMLHttpRequest();
+    return xmlhttp;
+};
+
+function updateMessage() {
+//	alert("41");
+get(appState.mainUrl + "?token=" + appState.token,
+function(answer) {
+//	alert("42");
+his = document.getElementById('history');
+//alert("43");
+var str = "";
+for (var i = 0; i < answer.messages.length; i++)
+str += "[" + answer.messages[i].id + "] " + answer.messages[i].user + ": " + answer.messages[i].message + "\n";
+his.value = str;
+//alert("44");
+});
+setTimeout(updateMessage, 1000);
+};
