620ccf7da050c6a7f4c9be7c676681d59cea976e
ZeddzZ <zhenya_dz@mail.ru>
2015-05-26 01:09:59 +0300

    Few updates for L13

--- Chat/MySQL/4.sql
+++ Chat/MySQL/4.sql
@@ -1,2 +1,2 @@
 INSERT into messages values ('12', 'hello, kitty','1111-11-11', '7');
-SELECT * FROM messages where text like '%hello%';
+SELECT * FROM messages where text like '%hello%' AND user_id 7;
